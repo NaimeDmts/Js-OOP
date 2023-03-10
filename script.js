6aa89c455b2468cbb3453af6889e175d81a3de31
NaimeDmts <naime.dmts@gmail.com>
2023-03-10 16:33:00 +0300

    3- Prototype

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
-// let soru = {
+// OOP: Nesne Tabanlı Programlama
 
-//     soruMetni: "Hangisi javascript paket yönetim uygulamasıdır",
-//     cevapSecenekleri:{
-//         a:  "Node.js",
-//         b:  "Typescript",
-//         c:  "Npm"
-//     },
-//     dogruCevap: "c",
-//     cevabiKontrolEt: function(cevap){
-//         return cevap === this.dogruCevap
-//     }
-// }
-// let soru2 = {
-
-//     soruMetni: "Hangisi .net paket yönetim uygulamasıdır",
-//     cevapSecenekleri:{
-//         a:  "Node.js",
-//         b:  "nuget",
-//         c:  "Npm"
-//     },
-//     dogruCevap: "b",
-//     cevabiKontrolEt: function(cevap){
-//         return cevap === this.dogruCevap
-//     }
-// }
-
-//Sınıf, constructor => nesne * 30
-//ES5, ES6, ES7
-
-function Soru(soruMetni, cevapSecenekleri, dogruCevap,cevabiKontrolEt){
+function Soru(soruMetni, cevapSecenekleri, dogruCevap){
     this.soruMetni = soruMetni;
     this.cevapSecenekleri = cevapSecenekleri;
     this.dogruCevap = dogruCevap;
-    this.cevabiKontrolEt = function(cevap){
-        return cevap === this.dogruCevap
-    }
+
+    console.log(this);
 }
+
+Soru.prototype.cevabiKontrolEt = function(cevap){
+    return cevap === this.dogruCevap
+}
+
 let soru1 = new Soru("Hangisi javascript paket yönetim uygulamasıdır",{ a:"Node.js", b:"Typescript", c:"Npm" },"c");
+console.log("Soru1 cevap doğru mu?",soru1.cevabiKontrolEt("c"));
 let soru2 = new Soru("Hangisi .net paket yönetim uygulamasıdır",{ a:"Node.js", b:"Nuget", c:"Npm" },"b");
 
 let sorular = [
@@ -45,9 +22,4 @@ let sorular = [
     new Soru("Hangisi javascript paket yönetim uygulamasıdır",{ a:"Node.js", b:"Typescript", c:"Npm" },"c"),
     new Soru("Hangisi .net paket yönetim uygulamasıdır",{ a:"Node.js", b:"Nuget", c:"Npm" },"b"),
 ]
-for(let s of sorular){
-    console.log(s.soruMetni); 
-    console.log(s.cevapSecenekleri);
-    console.log(s.dogruCevap);
-    console.log(s.cevabiKontrolEt("c"));
-}
+
