c02b3535a43eae3a1ebaff0a993b89f75d7ee742
NaimeDmts <naime.dmts@gmail.com>
2023-03-10 14:59:42 +0300

    1- Javascript OOP Nedir?

---
+++ script.js
@@ -0,0 +1,33 @@
+let soru = {
+
+    soruMetni: "Hangisi javascript paket yönetim uygulamasıdır",
+    cevapSecenekleri:{
+        a:  "Node.js",
+        b:  "Typescript",
+        c:  "Npm"
+    },
+    dogruCevap: "c",
+    cevabiKontrolEt: function(cevap){
+        return cevap === this.dogruCevap
+    }
+}
+let soru2 = {
+
+    soruMetni: "Hangisi .net paket yönetim uygulamasıdır",
+    cevapSecenekleri:{
+        a:  "Node.js",
+        b:  "nuget",
+        c:  "Npm"
+    },
+    dogruCevap: "b",
+    cevabiKontrolEt: function(cevap){
+        return cevap === this.dogruCevap
+    }
+}
+console.log(soru.soruMetni);
+console.log(soru.cevabiKontrolEt("c"));
+console.log(soru2.cevabiKontrolEt("b"));
+
+//Sınıf, constructor => nesne * 30
+//ES5, ES6, ES7
+
