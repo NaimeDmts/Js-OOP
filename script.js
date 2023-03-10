09905268dd029058c7457ae1a4038b96208443f6
NaimeDmts <naime.dmts@gmail.com>
2023-03-10 16:07:35 +0300

    2- Nesne Oluşturma: Constructor

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-let soru = {
+// let soru = {
 
-    soruMetni: "Hangisi javascript paket yönetim uygulamasıdır",
-    cevapSecenekleri:{
-        a:  "Node.js",
-        b:  "Typescript",
-        c:  "Npm"
-    },
-    dogruCevap: "c",
-    cevabiKontrolEt: function(cevap){
-        return cevap === this.dogruCevap
-    }
-}
-let soru2 = {
+//     soruMetni: "Hangisi javascript paket yönetim uygulamasıdır",
+//     cevapSecenekleri:{
+//         a:  "Node.js",
+//         b:  "Typescript",
+//         c:  "Npm"
+//     },
+//     dogruCevap: "c",
+//     cevabiKontrolEt: function(cevap){
+//         return cevap === this.dogruCevap
+//     }
+// }
+// let soru2 = {
 
-    soruMetni: "Hangisi .net paket yönetim uygulamasıdır",
-    cevapSecenekleri:{
-        a:  "Node.js",
-        b:  "nuget",
-        c:  "Npm"
-    },
-    dogruCevap: "b",
-    cevabiKontrolEt: function(cevap){
-        return cevap === this.dogruCevap
-    }
-}
-console.log(soru.soruMetni);
-console.log(soru.cevabiKontrolEt("c"));
-console.log(soru2.cevabiKontrolEt("b"));
+//     soruMetni: "Hangisi .net paket yönetim uygulamasıdır",
+//     cevapSecenekleri:{
+//         a:  "Node.js",
+//         b:  "nuget",
+//         c:  "Npm"
+//     },
+//     dogruCevap: "b",
+//     cevabiKontrolEt: function(cevap){
+//         return cevap === this.dogruCevap
+//     }
+// }
 
 //Sınıf, constructor => nesne * 30
 //ES5, ES6, ES7
 
+function Soru(soruMetni, cevapSecenekleri, dogruCevap,cevabiKontrolEt){
+    this.soruMetni = soruMetni;
+    this.cevapSecenekleri = cevapSecenekleri;
+    this.dogruCevap = dogruCevap;
+    this.cevabiKontrolEt = function(cevap){
+        return cevap === this.dogruCevap
+    }
+}
+let soru1 = new Soru("Hangisi javascript paket yönetim uygulamasıdır",{ a:"Node.js", b:"Typescript", c:"Npm" },"c");
+let soru2 = new Soru("Hangisi .net paket yönetim uygulamasıdır",{ a:"Node.js", b:"Nuget", c:"Npm" },"b");
+
+let sorular = [
+    new Soru("Hangisi javascript paket yönetim uygulamasıdır",{ a:"Node.js", b:"Typescript", c:"Npm" },"c"),
+    new Soru("Hangisi .net paket yönetim uygulamasıdır",{ a:"Node.js", b:"Nuget", c:"Npm" },"b"),
+    new Soru("Hangisi javascript paket yönetim uygulamasıdır",{ a:"Node.js", b:"Typescript", c:"Npm" },"c"),
+    new Soru("Hangisi .net paket yönetim uygulamasıdır",{ a:"Node.js", b:"Nuget", c:"Npm" },"b"),
+]
+for(let s of sorular){
+    console.log(s.soruMetni); 
+    console.log(s.cevapSecenekleri);
+    console.log(s.dogruCevap);
+    console.log(s.cevabiKontrolEt("c"));
+}
